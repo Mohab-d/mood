5bc9b8f3449763a70507752917a3be3ddc7c3e7a
Mohammad Mohab <mohab.swe@gmail.com>
2025-10-19 21:53:54 +0300

    Fix createTempId utility was not concatenating properly

--- src/core/utilities/createTempId.utility.ts
+++ src/core/utilities/createTempId.utility.ts
@@ -1,8 +1,8 @@
 export function createTempId(): string {
   let tempId = "";
 
-  tempId.concat("tempId_");
-  tempId.concat(String(Date.now()));
+  tempId = tempId.concat("tempId_");
+  tempId = tempId.concat(String(Date.now()));
 
   return tempId;
 }
